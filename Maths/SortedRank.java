54b514aa31cb6695fd8234b6c8c2d28a7f35725e
Abhishek Verma <32921037+AVerma475@users.noreply.github.com>
2020-05-16 21:13:00 +0530

    Create SortedRank.java

---
+++ Maths/SortedRank.java
@@ -0,0 +1,42 @@
+package Math;
+
+public class SortedRank {
+
+    int fact(int A){
+        if(A <= 1){
+            return A;
+        }
+
+        return A*fact(A-1)%1000003;
+    }
+
+    int smallerRight(String A, int low, int high){
+
+        char c = A.charAt(low);
+        int count = 0;
+        for(int i=low+1;i<=high;i++){
+            if(c > A.charAt(i)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public int findRank(String A) {
+
+        int rank = 1;
+
+        int countRight = 0;
+
+        for(int i=0;i<A.length();i++){
+
+            countRight = smallerRight(A, i, A.length()-1);
+            rank += (countRight*fact(A.length()-i-1));
+            rank = rank % 1000003;
+        }
+
+        return rank;
+    }
+
+}
