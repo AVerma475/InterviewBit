68cb8ac1911dcc5926c30bcce6679cb4bac657e3
Abhishek Verma <32921037+AVerma475@users.noreply.github.com>
2020-05-16 21:13:54 +0530

    Create VerifyPrime.java

---
+++ Maths/VerifyPrime.java
@@ -0,0 +1,19 @@
+package Math;
+
+
+public class VerifyPrime {
+    public int isPrime(int A) {
+
+        if( A == 0 || A == 1){
+            return 0;
+        }
+
+        for(int i=2; i*i <= A ;i++){
+            if(A%i == 0){
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+}
